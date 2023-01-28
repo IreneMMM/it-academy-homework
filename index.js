24b25debe950dedcf30e3ccc06f0ff6be7e29e2e
Irina <menshovaiv@gmail.com>
2023-01-28 23:46:58 +0300

    fixed error in lesson 4 hometask

--- index.js
+++ index.js
@@ -1,23 +1,15 @@
 const fs = require('fs-extra');
 
-fs.ensureDir('./folder1', () => {
-    fs.writeFileSync('./folder1/text.txt', 'This is a first file.', 'utf8', (error) => {
-        error ? console.log(error) : console.log('The file has been created');
-    });
+fs.ensureDirSync('./folder1', () => {});
+fs.ensureFileSync('./folder1/text.txt', 'This is a first file.', 'utf8', () => {});
+fs.ensureDirSync('./folder2', () => {});
+fs.moveSync('./folder1/text.txt', './folder2/text.txt');
 
-    fs.ensureDirSync('./folder2', () => {});
+fs.ensureDirSync('./folder3', () => {});
+fs.copySync('./folder2/text.txt', './folder3/text.txt');
 
-    fs.moveSync('./folder1/text.txt', './folder2/text.txt'), {
-        overwrite: true
-    };
-});
-
-
-fs.mkdir('./folder3', () => {
-    fs.copySync('./folder2/text.txt', './folder3/text.txt');
-    fs.removeSync('./folder2/text.txt');
-    fs.removeSync('./folder3/text.txt');
-    fs.removeSync('./folder1');
-    fs.removeSync('./folder2');
-    fs.removeSync('./folder3');
-});
+fs.removeSync('./folder2/text.txt');
+fs.removeSync('./folder3/text.txt');
+fs.removeSync('./folder1');
+fs.removeSync('./folder2');
+fs.removeSync('./folder3');
